f8b266a7e67db56a7a00e067c24285e0058cd75b
Muhammad Ammad <mammad@touchstone.com.pk>
2023-12-12 07:51:28 -0800

    Added some core features

--- client/src/components/common/Footer.js
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <div className="container">
-      Copyright © <small>{new Date().getFullYear()}</small> Developed by US
-    </div>
-  );
-};
-
-export default Footer;
