84cacf42a3870bf6f1786b5245a6a73524da3fbc
Muhammad Ammad <mammad@touchstone.com.pk>
2023-12-29 00:15:55 +0500

    Created Basic Front End Files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
+import Protected from './components/Protected';
+import AdminPage from './pages/admin/AdminPage';
+import { useState } from 'react';
+import Header from './components/Header';
+import BusDetails from './pages/BusDetails';
+import Booking from "./pages/client/Booking";
+
+
+
 
 function App() {
+  const [isLoggedIn, setisLoggedIn] = useState(true);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header />
+            <Routes>
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/signup" element={<SignupPage />} />
+              <Route path="/bus/:id" element={<BusDetails />} />
+              <Route path="/booking/:id" element={<Booking />} />
+              {/* <Route path='/admin'
+                element={
+                  <Protected isLoggedIn={isLoggedIn}>
+                    <AdminPage />
+                  </Protected>
+                }
+              /> */}
+            </Routes>
+        </Router>
     </div>
   );
 }
